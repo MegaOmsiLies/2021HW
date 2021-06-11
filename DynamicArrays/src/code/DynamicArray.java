3e33ae7b1d9715a637dde5cac566f9941a288711
MegaOmsiLies <super.sbrf@mail.ru>
2021-06-12 02:29:33 +0300

    DynamicArray

--- DynamicArrays/src/code/DynamicArray.java
+++ DynamicArrays/src/code/DynamicArray.java
@@ -1,5 +1,19 @@
 package code;
 
 public class DynamicArray {
+public class DynamicArray <T>{
+    private final int SIZE = 10;
+    private final int RATE = 2;
+    private T[] Arr;
 
-}
+    public DynamicArray(){
+        Arr = (T[])new Object[SIZE];
+
+    }
+
+    public DynamicArray(int firstSize){
+        if (firstSize > -1) {
+            Arr = (T[]) new Object[firstSize];
+        }
+        else {Arr = (T[])new Object[SIZE];}
+    }
--- DynamicArrays/src/tests/TestDynamicArray.java
+++ DynamicArrays/src/tests/TestDynamicArray.java
@@ -2,5 +2,16 @@ package tests;
 
 public class TestDynamicArray {
     
+import code.DynamicArray;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestDynamicArray extends Assertions {
+    @Test
+    public void test_resize_and_size(){
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.resize(1);
+        assertEquals(1, dynamicArray.capacity());
+    }
 }
 
