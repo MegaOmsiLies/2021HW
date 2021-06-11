85c0446d0c80d9866d65d4c4ffd14f53f1b91bfb
MegaOmsiLies <super.sbrf@mail.ru>
2021-06-12 02:58:10 +0300

    test findLast

--- DynamicArrays/src/tests/TestDynamicArray.java
+++ DynamicArrays/src/tests/TestDynamicArray.java
@@ -73,3 +73,13 @@ public class TestDynamicArray extends Assertions {
         assertEquals(-1, dynamicArray.findFirst(3));
     }
 
+    @Test
+    public void test_findLast(){
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.resize(3);
+        dynamicArray.set(0, 5);
+        dynamicArray.set(1, 2);
+        dynamicArray.set(2, 5);
+        assertEquals(2, dynamicArray.findLast(5));
+    }
+
