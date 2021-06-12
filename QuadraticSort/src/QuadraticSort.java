d1701f53c3c8398aee47c061e7a9403e2d364962
MegaOmsiLies <super.sbrf@mail.ru>
2021-06-12 03:40:39 +0300

    Task 7 complete

---
+++ QuadraticSort/src/QuadraticSort.java
@@ -0,0 +1,20 @@
+public class QuadraticSort {
+    public QuadraticSort() {
+    }
+
+    public void sort(int[] data) {
+        int n = data.length;
+
+        for(int i = 1; i < n; ++i) {
+            int key = data[i];
+
+            int j;
+            for(j = i - 1; j >= 0 && data[j] > key; --j) {
+                data[j + 1] = data[j];
+            }
+
+            data[j + 1] = key;
+        }
+
+    }
+}
