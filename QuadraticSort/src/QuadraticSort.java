399c90a030dfcab08a9c63f3e633481583fb7881
MegaOmsiLies <super.sbrf@mail.ru>
2021-06-12 03:57:20 +0300

    Task 7 complete

--- QuadraticSort/src/QuadraticSort.java
+++ QuadraticSort/src/QuadraticSort.java
@@ -2,10 +2,12 @@ import java.util.Arrays;
 
 
 public class QuadraticSort {
-    public void sort(int[] data) {
+    public void sort(int[] data)
+    {
         for (int i = 1; i < data.length; ++i) {
             int K = data[i];
             int j = i - 1;
+
             while (j >= 0 && K < data[K]) {
                 data[j + 1] = data[j];
                 j = j - 1;
